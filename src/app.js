4c2b541aa0c23cc9d96c7952df179ab03a7f9f0f
khushi-chauhan1 <kchauhan2111578@gmail.com>
2025-07-02 19:32:03 +0530

    exploring delete ,post ,get,basic routing

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
+const  fs = require('fs');
 const express = require('express');
 const app = express();
-
 const port=100;
-
-
-
-
-app.use("/",(req, res) => {
-    res.send('Hello, World!');
+const data=require('./user.json');
+app.get('/user', (req, res) => {
+  res.send(data); 
 });
-app.listen(port, () => { 
+app.post('/user',(req,res)=>{
+  console.log("Data is saved in daatbase");
+  res.send('User Created!');
+})
+app.delete('/user',(req,res)=>{
+  
+  res.send(`User with ID  deleted`);
+});
+app.patch('/user',(req,res)=>{
+  res.send(`User with ID updated`);
+});
+
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
+ 
