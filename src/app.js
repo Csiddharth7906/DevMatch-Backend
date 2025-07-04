48d43f29860580d91a38fee984ed88dd9ad340a0
Csiddharth7906 <siddharthchauhan7906@gmail.com>
2025-07-04 14:43:02 +0530

    DataBase Got Connecter

--- src/app.js
+++ src/app.js
@@ -1,30 +1,19 @@
-const fs =require("fs")
+
 const express = require('express');
+const connectDB = require('./config/database');
+const port = 3000;
 
 const app = express();
-const port=100;
-const data= require("./user.json")
 
-app.get("/user",(req,res)=>{
-  // try{
-    throw new Error("APTE ATATET TE TE TE TE TE TE TA")
-    res.send("USERDATA")
-  // }catch(err){
-  //   res.status(500).send("SOMETHINg WRONG")
-    
-  // }
-
-
-})
-app.use("/",(err,req,res,next)=>{
-  if(err){
-res.status(500).send("SOMETHINg WRONG")
-  }
-})
-
-
+connectDB().then(()=>{
+        console.log("Database is connected");
+        
 app.listen(port, () => { 
   console.log(`Server is running on http://localhost:${port}`);
 });
 
+         
+}).catch(err=>{
+  console.error("Error connecting to database");
+})
  
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+
+const connectDB = async ()=>{
+
+  await   mongoose.connect(
+        "mongodb+srv://siddharthchauhan_0:Siddharth0@cluster0.y4bqnrh.mongodb.net/devMatch"
+    )
+}
+ 
+module.exports = connectDB;
+  
