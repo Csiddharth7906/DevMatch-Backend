b89e5d75ef4e5cdd82d7aa52aadd838fdbd5649c
Csiddharth7906 <siddharthchauhan7906@gmail.com>
2025-07-18 10:10:36 +0530

    fix user get request recived data

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,15 +3,18 @@ const { userAuth } = require("../middleware/auth");
 const userRouter = express.Router();
 const connectionRequest = require("../models/connectionRequest")
 //Get all the pending connection request for the LoggedIn user
-userRouter.get("/user/reques/received",userAuth, async (req,res)=>{
+userRouter.get("/user/requests/received",userAuth, async (req,res)=>{
     try {
         const loggedInUser = req.user;
         const connectionRequests = await connectionRequest.find({
             toUserId : loggedInUser._id,
-            status: intersted
-         })
+            status: "interested"
+            
+         });
+        res.status(200).json(connectionRequests);
     } catch (err) {
         res.status(400).send("Error: "+err.message)
     }
-})
+});
+
 module.exports = userRouter;
