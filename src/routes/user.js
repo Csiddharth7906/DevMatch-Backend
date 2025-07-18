e65c79a72b030bfba3d52d246260adff00108e13
Csiddharth7906 <siddharthchauhan7906@gmail.com>
2025-07-18 20:09:00 +0530

    created feed api with proper checks

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const userRouter = express.Router();
+const User = require("../models/user");
 const connectionRequest = require("../models/connectionRequest")
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 //Get all the pending connection request for the LoggedIn user
@@ -48,5 +49,35 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         res.status(400).send("Error: " + err.message)
     }
 });
+//Get all the feed api data for the logged in user
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try{
+       
+         const loggedInUser = req.user;
+         //find all connection request (sent + recived)
+         const connectionRequests = await connectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+         }).select("fromUserId toUserId");
+         const hideUsersFromFeed = new Set();
+         connectionRequests.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+         });
+         const users = await User.find({
+            $and : [
+                { _id : { $nin: Array.from(hideUsersFromFeed) }},// Exclude users who are in connection requests array from the feed 
+                { _id : { $ne: loggedInUser._id }}// Exclude the logged in user from the feed
+         ]   
+         }) .select(USER_SAFE_DATA).limit(10);
+
+     res.send(users);
+
+    }catch(err){
+        res.status(400).json({message: "Error: "+err.message});
+    }
+})
 
 module.exports = userRouter;
